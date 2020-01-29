3d3b01ab7c41f9a4246a23dd9bb369c41aab096e
julesxana <dunlapj@mail.usf.edu>
2020-01-29 16:27:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Deliverable2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter an integer between 1 and 100");
+            //string input = Console.ReadLine();
+            int value = int.Parse(Console.ReadLine());
+
+            for(int count = 1; count <= value; count++)
+            {
+                Console.WriteLine("You have entered " + value + " This is the current integer value in the loop " + count);
+            }
+        }
+    }
+}

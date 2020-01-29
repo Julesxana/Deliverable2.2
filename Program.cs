61d9c1355dffc09470c33a304f5a2f59be6972fc
julesxana <dunlapj@mail.usf.edu>
2020-01-29 16:32:11 -0500

    Added comments to improve readability

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/* 
+Author: Juliana Dunlap
+Date: 1/30/2020
+Project: Week 2 Deliverable 2.2 Iteration Statements
+*/
+using System;
 
 namespace Deliverable2._2
 {
@@ -6,13 +11,17 @@ namespace Deliverable2._2
     {
         static void Main(string[] args)
         {
-            
+            // Ask user for an iteration value
             Console.WriteLine("Please enter an integer between 1 and 100");
-            //string input = Console.ReadLine();
+
+            //the variable value is the integer the user enterd that will determine the number of iterations
             int value = int.Parse(Console.ReadLine());
+
+            //the vairable count is how many iterations the loop has completed, incrementing by 1
 
             for(int count = 1; count <= value; count++)
             {
+                //the loop tells the user the number entered and what iteration the loop is on every cycle
                 Console.WriteLine("You have entered " + value + " This is the current integer value in the loop " + count);
             }
         }

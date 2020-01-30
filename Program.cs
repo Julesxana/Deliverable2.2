3c204691af8e2bb7845b13e28bd6198b000b366e
julesxana <dunlapj@mail.usf.edu>
2020-01-30 17:54:22 -0500

    bettered coding style

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace Deliverable2._2
             try
             {
 
-                
                 // Ask user for an iteration value
                 Console.WriteLine("Please enter an integer between 1 and 100");
 
@@ -23,16 +22,16 @@ namespace Deliverable2._2
 
                 //the vairable count is how many iterations the loop has completed, incrementing by 1
 
-                if ((value >= 100) && (value <= 1))
-
-                for (int count = 1; count <= value; count++)
+                if ((value <= 100) && (value >= 1))
                 {
-                    //the loop tells the user the number entered and what iteration the loop is on every cycle
-                    Console.WriteLine("You have entered " + value + " This is the current integer value in the loop " + count);
+                    for (int count = 1; count <= value; count++)
+                    {
+                        //the loop tells the user the number entered and what iteration the loop is on every cycle
+                        Console.WriteLine("You have entered " + value + " This is the current integer value in the loop " + count);
+                    } 
                 }
                 else
                     Console.WriteLine("please enter an integer between 1 and 100");
-
             }
             catch
             {
